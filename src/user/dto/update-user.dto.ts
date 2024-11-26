b3708238b854d0de846d76e882bd9f00afb39e00
Sergey Denisenko <sdenisenko@five9.com>
2024-11-26 01:25:17 +0000

    fix: add missed files.

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,11 +1,15 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { PartialType } from '@nestjs/swagger';
+import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
-export class UpdatePasswordDto {
+export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsString()
   @IsNotEmpty()
-  readonly oldPassword: string;
+  oldPassword: string;
 
   @IsString()
   @IsNotEmpty()
-  readonly newPassword: string;
+  @MinLength(1, { message: 'The password must contain at least 1 characters' })
+  @MaxLength(15, { message: 'The password must not exceed 15 characters' })
+  newPassword: string;
 }
